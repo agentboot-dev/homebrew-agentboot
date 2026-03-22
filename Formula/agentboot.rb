222484efa8fbba197ffdbc2cba6eab5639cdac0e
Mike Saavedra <saavyone@gmail.com>
2026-03-21 23:17:43 -0400

    agentboot 0.3.0

--- Formula/agentboot.rb
+++ Formula/agentboot.rb
@@ -1,8 +1,8 @@
 class Agentboot < Formula
   desc "Convention over configuration for agentic development teams"
   homepage "https://agentboot.dev"
-  url "https://registry.npmjs.org/agentboot/-/agentboot-0.2.0.tgz"
-  sha256 "da76e917fc6f640180c02b72f98f4e64b7ce0c0e427412f94d9df7a11ed4e501"
+  url "https://registry.npmjs.org/agentboot/-/agentboot-0.3.0.tgz"
+  sha256 "ccb3be20d19b2e790f39189fe3c944f994a1279f917e892bfe4b6ec2359544e9"
   license "Apache-2.0"
 
   depends_on "node@22"
